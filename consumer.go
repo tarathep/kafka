ee1b1707bde54eada63a87083e8c77b8fbd3e967
tarathep <tarathec@ais.co.th>
2019-10-29 17:30:55 +0700

    add consumer

--- consumer.go
+++ consumer.go
@@ -1,4 +1,69 @@
 package kafka
 
+import (
+	"fmt"
+
+	"github.com/Shopify/sarama"
+)
+
 //Consumer is ..
 type Consumer struct{}
+
+func Subscribe(brokerAddress string, topic string) {
+
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
+
+	// Specify brokers address. This is default one
+	brokers := []string{brokerAddress}
+
+	// Create new consumer
+	master, err := sarama.NewConsumer(brokers, config)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := master.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	//topic := "user-created"
+	//topic := "aaa"
+	// How to decide partition, is it fixed value...?
+	consumer, err := master.ConsumePartition(topic, 0, sarama.OffsetOldest)
+	if err != nil {
+		panic(err)
+	}
+
+	//signals := make(chan os.Signal, 1)
+	//signal.Notify(signals, os.Interrupt)
+
+	// Count how many message processed
+	msgCount := 0
+
+	// Get signnal for finish
+	doneCh := make(chan struct{})
+
+	go func() {
+		for {
+			select {
+			case err := <-consumer.Errors():
+				fmt.Println(err)
+			case msg := <-consumer.Messages():
+				msgCount++
+				fmt.Println("Received messages" + string(msg.Key) + " VALUE ==>" + string(msg.Value))
+			}
+			/*
+				case <-signals:
+					fmt.Println("Interrupt is detected")
+					doneCh <- struct{}{}
+				}
+			*/
+		}
+	}()
+
+	<-doneCh
+	fmt.Println("Processed", msgCount, "messages")
+}
